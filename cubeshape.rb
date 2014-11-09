5788ad8c178a59a77d2b1ac509230f965d82f6b4
zs <baloghzsof@gmail.com>
2014-11-09 16:40:37 +0100

    Color options & side stickers.

--- cubeshape.rb
+++ cubeshape.rb
@@ -7,8 +7,9 @@ Cuba.define do
   on get do
     on 'cubeshape/:shape' do |shape|
       size = req.params['size'] || 200
+      colors = req.params['colors'].to_s.scan(/[a-z]/)
       res.headers['Content-Type']  = 'image/png'
-      res.write Painter.new(size.to_i).shape_blob(shape)
+      res.write Painter.new(size.to_i).shape_blob(shape, colors)
     end
 
     on root do
